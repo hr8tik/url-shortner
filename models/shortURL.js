8cdf6a4952fa36d903fd318a48dc9c20631c6353
Kshitij841 <kshitijsingh841@gmail.com>
2021-08-26 15:12:09 +0530

    v2

--- models/shortURL.js
+++ models/shortURL.js
@@ -1,22 +1,21 @@
 const mongoose = require('mongoose')
 const shortId = require('shortid')
 
-
-const shortURLSchema = new mongoose.Schema({
-    full:{
-        type: String,
-        require: true
-    },
-    short: {
-        type: String,
-        required: true,
-        default: shortId.generate
-    },
-    clicks: {
-        type: Number,
-        required: true,
-        default: 0
-    }
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+    required: true
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0
+  }
 })
 
-module.exports = mongoose.model('ShortURL', shortURLSchema)
+module.exports = mongoose.model('ShortUrl', shortUrlSchema)
--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const ShortURL = require('./models/shortURL')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-mongoose.connect('mongodb://localhost/urlShortner', {
-    useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
 })
 
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
 })
 
-app.post('/shortURLs', (req,res) =>{
-    ShortURL.create({full: req.body.fullURL})
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
 })
 
 app.listen(process.env.PORT || 5000);
